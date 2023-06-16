a47b71ed4661bfa2cb97cb578174d775569bad0b
Anh Thắng <anhthang2529@gmail.com>
2023-06-17 00:43:23 +0700

    Partital View

---
+++ Sovereign/Sovereign/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,21 @@
+﻿using Sovereign.Models.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sovereign.Controllers
+{
+    public class LoaiSanPhamController : Controller
+    {
+        // GET: LoaiSanPham
+        public ActionResult Index(String id)
+        {
+            var ds = LoaiSanPhamBus.ChiTiet(id);
+            return View(ds);
+        }
+
+
+    }
+}
---
+++ Sovereign/Sovereign/Models/BUS/LoaiSanPhamBus.cs
@@ -0,0 +1,23 @@
+﻿using SovereignConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sovereign.Models.BUS
+{
+    public class LoaiSanPhamBus
+    {
+        public static IEnumerable<Category> DanhSach()
+        {
+            var db = new SovereignConnectionDB();
+            return db.Query<Category>("select * from Category");
+        }
+
+        public static IEnumerable<Product> ChiTiet(String id)
+        {
+            var db = new SovereignConnectionDB();
+            return db.Query<Product>("select * from Product where Category_Id = '"+id+"'");
+        }
+    }
+}
